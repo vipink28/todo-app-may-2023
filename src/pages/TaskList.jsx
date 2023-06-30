8fd9769427a7d1626d3b49a7b4a31475091b8df2
vipink28 <vipinkalyan28@gmail.com>
2023-06-30 09:14:57 +0530

    popup created

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -2,6 +2,10 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import TaskContext from '../context/TaskContext';
 import { dateFormat } from '../helper';
+import Popup from '../components/Popup';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-regular-svg-icons';
+import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 function TaskList(props) {
     const {taskList}=useContext(TaskContext);
@@ -32,13 +36,27 @@ function TaskList(props) {
                                     <td>{item.title}</td>
                                     <td>{item.description}</td>
                                     <td>{dateFormat(item.duedate)}</td>
-                                    <td></td>
+                                    <td>
+                                        <span className='px-2' data-bs-toggle="modal" data-bs-target="#task-modal">
+                                            <FontAwesomeIcon icon={faEye} />
+                                        </span>
+
+                                        <span className='px-2' data-bs-toggle="modal" data-bs-target="#task-modal">
+                                            <FontAwesomeIcon icon={faPenToSquare} />
+                                        </span>
+
+                                        <span className='px-2' data-bs-toggle="modal" data-bs-target="#task-modal">
+                                            <FontAwesomeIcon icon={faTrashCan} />
+                                        </span>
+                                    </td>
                                 </tr>
                             )
                         }): <tr><td>No Data</td></tr>
                     }
                 </tbody>
             </table>
+            
+            <Popup />
         </div>
     );
 }
